67f2159411444eac1325ab1395dc30659c5f16f5
Jaeyoung Lee <jylee0121@gmail.com>
2017-04-11 10:08:53 +0000

    setting

--- viptechbot/settings.py
+++ viptechbot/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'viptechbot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgres_psycopg2',
+        'NAME': 'viptechbot',
+        'USER': 'viptech',
+        'PASSWORD': 'xxxxxxxx',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -118,3 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
